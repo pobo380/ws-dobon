31988f3e4027cd6d57cfce2accae01a998928a79
pobo380 <n3560k@gmail.com>
2011-09-26 05:21:16 +0900

    モデルの定義を追加

--- ws-dobon.rb
+++ ws-dobon.rb
@@ -5,16 +5,77 @@ require 'sinatra'
 
 ### Configs
 
-# cookie setting.
 use Rack::Session::Cookie,
   :expire_after => 2592000,
   :secret => 'change_me'
 
 ### Models
+module Models
 
-# connect to database.
-Sequel::Model.plugin(:schema)
-Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://db/database.sqlite3')
+  ## Connect to the database.
+  Sequel::Model.plugin(:schema)
+  Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://db/database.sqlite3')
+
+  ## Helper module
+  module AutoTimestamp
+    def before_create
+      self.created_at ||= Time.now
+      super
+    end
+  end
+
+  class Room < Sequel::Model
+    one_to_many :players
+    one_to_many :games
+
+    include AutoTimestamp
+  end
+
+  class Game < Sequel::Model
+    one_to_many :rounds
+    one_to_many :game_results
+
+    many_to_one :room
+
+    include AutoTimestamp
+  end
+
+  class GameResult < Sequel::Model
+    many_to_one :game
+    many_to_one :player
+
+    include AutoTimestamp
+  end
+
+  class Round < Sequel::Model
+    one_to_many :round_results
+
+    many_to_one :game
+    many_to_one :winner, :class => :Player
+    many_to_one :loser,  :class => :Player
+
+    include AutoTimestamp
+  end
+
+  class RoundResult < Sequel::Model
+    many_to_one :round
+    many_to_one :player
+
+    include AutoTimestamp
+  end
+
+  class Player < Sequel::Model
+    one_to_many :round_results
+    one_to_many :winner_rounds, :class => :Round, :key => :winner_id
+    one_to_many :loser_rounds,  :class => :Round, :key => :loser_id
+    one_to_many :game_results
+
+    many_to_one :room
+
+    include AutoTimestamp
+  end
+end
+
 
 ### Routes
 #
