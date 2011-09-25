bc1838d18dcfe0b8f63d0b1d632ecd1a8ff8f2f8
pobo380 <n3560k@gmail.com>
2011-09-26 04:53:26 +0900

    DB接続のコードを追加

--- ws-dobon.rb
+++ ws-dobon.rb
@@ -3,20 +3,21 @@ require 'rubygems'
 require 'sequel'
 require 'sinatra'
 
-#
-# Configs
-#
+### Configs
 
+# cookie setting.
+use Rack::Session::Cookie,
+  :expire_after => 2592000,
+  :secret => 'change_me'
 
-#
-# Models
-#
+### Models
 
+# connect to database.
+Sequel::Model.plugin(:schema)
+Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://db/database.sqlite3')
 
+### Routes
 #
-# Routes
-#
-
 get '/' do
   '日本語'
 end
