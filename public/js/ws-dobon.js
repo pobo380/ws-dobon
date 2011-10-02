5e0e7160fe3650082c34593ad5d5d98361c70d55
pobo380 <n3560k@gmail.com>
2011-10-03 01:55:23 +0900

    * トランプの画像表示を実装

--- public/js/ws-dobon.js
+++ public/js/ws-dobon.js
@@ -15,7 +15,7 @@ $(function() {
     $("#dialog-message").text(msg);
     $("#dialog-message").dialog({
       modal: true,
-      buttons: { Ok: function() {
+      buttons: { OK: function() {
         $(this).dialog("close");
         if(callback != undefined) { callback(); }
       }}
@@ -23,7 +23,13 @@ $(function() {
   };
 
   var update_hand = function(deck) {
-    $("#your_hand").text(deck.join(', '));
+    html = '';
+    for(var idx = 0; idx < deck.length; ++idx) {
+      pos = string2image(deck[idx]);
+      html = html + '<div class="playing_card_img" style="background-position:' + 
+             [pos.x, pos.y].join('px ') + 'px">'+ deck[idx] + '</div>';
+    }
+    $("#hand").html(html);
   };
 
   var update_players = function(order) {
@@ -40,6 +46,46 @@ $(function() {
     $("#game_table_container")
       .show(500);
   };
+
+  /** playing cards: string to image
+   */
+  var string2image = function(str) {
+    var pos  = {x:0, y:0};
+    var size = {w:258, h: 348}
+    suit = str.charAt(0);
+
+    rate = 0;
+    switch(suit) {
+      case 'H':
+        rate = 0; break;
+      case 'C':
+        rate = 1; break;
+      case 'D':
+        rate = 2; break;
+      case 'S':
+        rate = 3; break;
+      case 'F':
+        break;
+    }
+    pos.y = (size.h + 7) * rate * -1;
+
+    num = str.charAt(1);
+    rate = 0;
+    num_list = [
+      '1', '2', '3', '4',
+      '5', '6', '7', '8',
+      '9', '0', 'J', 'Q', 'K'
+    ];
+    for(var i = 0; i < num_list.length; ++i) {
+      if(num_list[i] == num) {
+        rate = i;
+        break;
+      }
+    }
+    pos.x = (size.w + 7) * rate * -1;
+
+    return pos;
+  }
 
   /** call ws-dobon APIs
    */
@@ -65,7 +111,6 @@ $(function() {
                });
              });
   });
-
 
   $("#join_room").bind('click', function() {
     call_api("player/join",
